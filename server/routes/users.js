34ef56dd204f8a2b3edb94442aa9758ee344f0d3
menahemc <menachem1988@gmail.com>
2018-10-22 15:18:19 +0300

    Add files via upload

---
+++ server/routes/users.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const JSON2 = require('circular-json');
+
+
+router.get('/', function (req, res) {
+
+  // Requires official Node.js MongoDB Driver 3.0.0+
+  var mongodb = require("mongodb");
+  var ObjectID = mongodb.ObjectID;
+
+  var client = mongodb.MongoClient;
+  var url = "mongodb://localhost:27017/";
+
+  client.connect(url, function (err, client) {
+
+    var db = client.db("MyFirstApp");
+    var collection = db.collection("users");
+
+    var query = {};
+
+    var cursor = collection.find(query);
+
+    var users = [];
+    var counter = 0;
+    cursor.forEach(function (u) {
+      users.push(u);
+      console.log(u);
+      counter++;
+      if (counter === 2) {
+        console.log(users);
+        const json = JSON2.stringify(users);
+        res.json(json);
+      }
+    });
+  });
+})
+
+module.exports = router;
